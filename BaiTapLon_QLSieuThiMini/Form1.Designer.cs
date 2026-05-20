5bdefec99fc0aaec3aa84df30901def109640b48
Quang Minh <quangminh5a2@gmail.com>
2026-05-20 21:28:34 +0700

    Lam xong giao dien tab

--- BaiTapLon_QLSieuThiMini/Form1.Designer.cs
+++ BaiTapLon_QLSieuThiMini/Form1.Designer.cs
@@ -28,44 +28,394 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            tabNhapHang = new TabPage();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            tableKhoHang = new DataGridView();
+            ColumnMaSP = new DataGridViewTextBoxColumn();
+            ColumnTenSP = new DataGridViewTextBoxColumn();
+            ColumnForSoLuong = new DataGridViewTextBoxColumn();
+            label6 = new Label();
+            label7 = new Label();
+            tableLichSuNhap = new DataGridView();
+            CollumnForMaSP = new DataGridViewTextBoxColumn();
+            CollumnForTenSP = new DataGridViewTextBoxColumn();
+            CollumnForThanhTien = new DataGridViewTextBoxColumn();
+            CollumnForNgay = new DataGridViewTextBoxColumn();
+            label5 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            textBoxForDonGia = new TextBox();
+            textBoxForTenSP = new TextBox();
+            textBoxForSoLuong = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBoxForMaSP = new TextBox();
+            buttonAdd = new Button();
+            tabControl1.SuspendLayout();
+            tabNhapHang.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)tableKhoHang).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)tableLichSuNhap).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // tabControl1
             // 
-            button1.Location = new Point(301, 189);
-            button1.Name = "button1";
-            button1.Size = new Size(264, 181);
-            button1.TabIndex = 0;
-            button1.Text = "NUTRATTO";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabNhapHang);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1315, 723);
+            tabControl1.TabIndex = 5;
             // 
-            // button2
+            // tabPage1
             // 
-            button2.Location = new Point(243, 82);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 29);
-            button2.TabIndex = 1;
-            button2.Text = "NutMoi";
-            button2.UseVisualStyleBackColor = true;
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1307, 690);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1307, 690);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabNhapHang
+            // 
+            tabNhapHang.AutoScroll = true;
+            tabNhapHang.BackColor = Color.LightGray;
+            tabNhapHang.Controls.Add(tableLayoutPanel2);
+            tabNhapHang.Controls.Add(label5);
+            tabNhapHang.Controls.Add(tableLayoutPanel1);
+            tabNhapHang.Location = new Point(4, 29);
+            tabNhapHang.Name = "tabNhapHang";
+            tabNhapHang.Padding = new Padding(3);
+            tabNhapHang.Size = new Size(1307, 690);
+            tabNhapHang.TabIndex = 2;
+            tabNhapHang.Text = "Nhập hàng & thống kê";
+            tabNhapHang.Click += tabPage3_Click;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel2.ColumnCount = 1;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.Controls.Add(tableKhoHang, 0, 1);
+            tableLayoutPanel2.Controls.Add(label6, 0, 0);
+            tableLayoutPanel2.Controls.Add(label7, 0, 2);
+            tableLayoutPanel2.Controls.Add(tableLichSuNhap, 0, 3);
+            tableLayoutPanel2.Location = new Point(11, 223);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 4;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 40F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 40F));
+            tableLayoutPanel2.Size = new Size(1290, 433);
+            tableLayoutPanel2.TabIndex = 12;
+            // 
+            // tableKhoHang
+            // 
+            tableKhoHang.AllowUserToAddRows = false;
+            tableKhoHang.AllowUserToDeleteRows = false;
+            tableKhoHang.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            tableKhoHang.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            tableKhoHang.Columns.AddRange(new DataGridViewColumn[] { ColumnMaSP, ColumnTenSP, ColumnForSoLuong });
+            tableKhoHang.Dock = DockStyle.Fill;
+            tableKhoHang.GridColor = SystemColors.HighlightText;
+            tableKhoHang.Location = new Point(3, 46);
+            tableKhoHang.Name = "tableKhoHang";
+            tableKhoHang.ReadOnly = true;
+            tableKhoHang.RowHeadersWidth = 51;
+            tableKhoHang.Size = new Size(1284, 167);
+            tableKhoHang.TabIndex = 9;
+            tableKhoHang.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // ColumnMaSP
+            // 
+            ColumnMaSP.HeaderText = "Mã sản phẩm";
+            ColumnMaSP.MinimumWidth = 6;
+            ColumnMaSP.Name = "ColumnMaSP";
+            ColumnMaSP.ReadOnly = true;
+            // 
+            // ColumnTenSP
+            // 
+            ColumnTenSP.HeaderText = "Tên sản phẩm";
+            ColumnTenSP.MinimumWidth = 6;
+            ColumnTenSP.Name = "ColumnTenSP";
+            ColumnTenSP.ReadOnly = true;
+            // 
+            // ColumnForSoLuong
+            // 
+            ColumnForSoLuong.HeaderText = "Số lượng tồn";
+            ColumnForSoLuong.MinimumWidth = 6;
+            ColumnForSoLuong.Name = "ColumnForSoLuong";
+            ColumnForSoLuong.ReadOnly = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Dock = DockStyle.Bottom;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(3, 15);
+            label6.Name = "label6";
+            label6.Size = new Size(1284, 28);
+            label6.TabIndex = 8;
+            label6.Text = "Kho hàng: ";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Dock = DockStyle.Bottom;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(3, 231);
+            label7.Name = "label7";
+            label7.Size = new Size(1284, 28);
+            label7.TabIndex = 10;
+            label7.Text = "Lịch sử nhập hàng:";
+            // 
+            // tableLichSuNhap
+            // 
+            tableLichSuNhap.AllowUserToAddRows = false;
+            tableLichSuNhap.AllowUserToDeleteRows = false;
+            tableLichSuNhap.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLichSuNhap.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            tableLichSuNhap.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            tableLichSuNhap.Columns.AddRange(new DataGridViewColumn[] { CollumnForMaSP, CollumnForTenSP, CollumnForThanhTien, CollumnForNgay });
+            tableLichSuNhap.GridColor = SystemColors.HighlightText;
+            tableLichSuNhap.Location = new Point(3, 262);
+            tableLichSuNhap.Name = "tableLichSuNhap";
+            tableLichSuNhap.ReadOnly = true;
+            tableLichSuNhap.RowHeadersWidth = 51;
+            tableLichSuNhap.Size = new Size(1284, 168);
+            tableLichSuNhap.TabIndex = 11;
+            tableLichSuNhap.CellContentClick += dataGridView2_CellContentClick;
+            // 
+            // CollumnForMaSP
+            // 
+            CollumnForMaSP.HeaderText = "Mã sản phẩm";
+            CollumnForMaSP.MinimumWidth = 6;
+            CollumnForMaSP.Name = "CollumnForMaSP";
+            CollumnForMaSP.ReadOnly = true;
+            // 
+            // CollumnForTenSP
+            // 
+            CollumnForTenSP.HeaderText = "Tên sản phẩm";
+            CollumnForTenSP.MinimumWidth = 6;
+            CollumnForTenSP.Name = "CollumnForTenSP";
+            CollumnForTenSP.ReadOnly = true;
+            // 
+            // CollumnForThanhTien
+            // 
+            CollumnForThanhTien.HeaderText = "Thành tiền nhập hàng";
+            CollumnForThanhTien.MinimumWidth = 6;
+            CollumnForThanhTien.Name = "CollumnForThanhTien";
+            CollumnForThanhTien.ReadOnly = true;
+            // 
+            // CollumnForNgay
+            // 
+            CollumnForNgay.HeaderText = "Ngày nhập hàng";
+            CollumnForNgay.MinimumWidth = 6;
+            CollumnForNgay.Name = "CollumnForNgay";
+            CollumnForNgay.ReadOnly = true;
+            // 
+            // label5
+            // 
+            label5.Dock = DockStyle.Top;
+            label5.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(3, 3);
+            label5.Name = "label5";
+            label5.Size = new Size(1301, 31);
+            label5.TabIndex = 7;
+            label5.Text = "Nhập sản phẩm mới vào kho";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            label5.Click += label5_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.ColumnCount = 4;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 15F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 35F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 15F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 35F));
+            tableLayoutPanel1.Controls.Add(textBoxForDonGia, 3, 1);
+            tableLayoutPanel1.Controls.Add(textBoxForTenSP, 1, 1);
+            tableLayoutPanel1.Controls.Add(textBoxForSoLuong, 3, 0);
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(label2, 2, 1);
+            tableLayoutPanel1.Controls.Add(label3, 0, 1);
+            tableLayoutPanel1.Controls.Add(label4, 2, 0);
+            tableLayoutPanel1.Controls.Add(textBoxForMaSP, 1, 0);
+            tableLayoutPanel1.Controls.Add(buttonAdd, 0, 2);
+            tableLayoutPanel1.ForeColor = SystemColors.ControlText;
+            tableLayoutPanel1.Location = new Point(6, 49);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.Size = new Size(1202, 182);
+            tableLayoutPanel1.TabIndex = 6;
+            // 
+            // textBoxForDonGia
+            // 
+            textBoxForDonGia.Dock = DockStyle.Fill;
+            textBoxForDonGia.Location = new Point(783, 63);
+            textBoxForDonGia.Name = "textBoxForDonGia";
+            textBoxForDonGia.Size = new Size(416, 27);
+            textBoxForDonGia.TabIndex = 8;
+            // 
+            // textBoxForTenSP
+            // 
+            textBoxForTenSP.Dock = DockStyle.Fill;
+            textBoxForTenSP.Location = new Point(183, 63);
+            textBoxForTenSP.Name = "textBoxForTenSP";
+            textBoxForTenSP.Size = new Size(414, 27);
+            textBoxForTenSP.TabIndex = 7;
+            // 
+            // textBoxForSoLuong
+            // 
+            textBoxForSoLuong.Dock = DockStyle.Fill;
+            textBoxForSoLuong.Location = new Point(783, 3);
+            textBoxForSoLuong.Name = "textBoxForSoLuong";
+            textBoxForSoLuong.Size = new Size(416, 27);
+            textBoxForSoLuong.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Top;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(174, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Mã sản phẩm: ";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Top;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.Location = new Point(603, 60);
+            label2.Name = "label2";
+            label2.Size = new Size(174, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Đơn giá: ";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Top;
+            label3.Font = new Font("Segoe UI", 12F);
+            label3.Location = new Point(3, 60);
+            label3.Name = "label3";
+            label3.Size = new Size(174, 28);
+            label3.TabIndex = 3;
+            label3.Text = "Tên sản phẩm: ";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Dock = DockStyle.Top;
+            label4.Font = new Font("Segoe UI", 12F);
+            label4.Location = new Point(603, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(174, 28);
+            label4.TabIndex = 1;
+            label4.Text = "Số lượng: ";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBoxForMaSP
+            // 
+            textBoxForMaSP.Dock = DockStyle.Fill;
+            textBoxForMaSP.Location = new Point(183, 3);
+            textBoxForMaSP.Name = "textBoxForMaSP";
+            textBoxForMaSP.Size = new Size(414, 27);
+            textBoxForMaSP.TabIndex = 4;
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.Dock = DockStyle.Fill;
+            buttonAdd.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            buttonAdd.Location = new Point(10, 130);
+            buttonAdd.Margin = new Padding(10);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Padding = new Padding(5);
+            buttonAdd.Size = new Size(160, 42);
+            buttonAdd.TabIndex = 9;
+            buttonAdd.Text = "Nhập hàng";
+            buttonAdd.UseVisualStyleBackColor = true;
+            buttonAdd.Click += buttonAdd_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(1315, 723);
+            Controls.Add(tabControl1);
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabNhapHang.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)tableKhoHang).EndInit();
+            ((System.ComponentModel.ISupportInitialize)tableLichSuNhap).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Button button1;
-        private Button button2;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabNhapHang;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TextBox textBoxForDonGia;
+        private TextBox textBoxForTenSP;
+        private TextBox textBoxForSoLuong;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBoxForMaSP;
+        private Label label5;
+        private DataGridView tableKhoHang;
+        private Label label6;
+        private DataGridViewTextBoxColumn ColumnMaSP;
+        private DataGridViewTextBoxColumn ColumnTenSP;
+        private DataGridViewTextBoxColumn ColumnForSoLuong;
+        private DataGridView tableLichSuNhap;
+        private Label label7;
+        private DataGridViewTextBoxColumn CollumnForMaSP;
+        private DataGridViewTextBoxColumn CollumnForTenSP;
+        private DataGridViewTextBoxColumn CollumnForThanhTien;
+        private DataGridViewTextBoxColumn CollumnForNgay;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Button buttonAdd;
     }
 }
--- BaiTapLon_QLSieuThiMini/Form1.cs
+++ BaiTapLon_QLSieuThiMini/Form1.cs
@@ -1,4 +1,4 @@
-namespace BaiTapLon_QLSieuThiMini
+﻿namespace BaiTapLon_QLSieuThiMini
 {
     public partial class Form1 : Form
     {
@@ -10,6 +10,46 @@ namespace BaiTapLon_QLSieuThiMini
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {   
+
+            if (string.IsNullOrWhiteSpace(textBoxForMaSP.Text)
+                || string.IsNullOrWhiteSpace(textBoxForTenSP.Text)
+                || string.IsNullOrWhiteSpace(textBoxForSoLuong.Text)
+                || string.IsNullOrWhiteSpace(textBoxForDonGia.Text))
+            {
+                MessageBox.Show("Vui lòng nhập dữ liệu!", "Cảnh báo", MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                return;
+            }
+            tableKhoHang.Rows.Add(textBoxForMaSP.Text, textBoxForTenSP.Text, textBoxForSoLuong.Text, textBoxForDonGia.Text);
+            tableLichSuNhap.Rows.Add(textBoxForMaSP.Text, textBoxForTenSP.Text, textBoxForSoLuong.Text, textBoxForDonGia.Text);
         }
     }
 }
